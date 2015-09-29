c2e7179f2b0ce60aaba1bf992b0acc2919cff4ec
Jonathan Lee <j47lee@gmail.com>
2015-09-29 16:48:56 -0700

    show, add, remove working

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,10 +2,36 @@ var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log('App Controller working properly');
 
+var refresh = function(){
     $http.get('contactList').success(function(res){
       console.log('Data received from GET request');
-      $scope.contactList = res
+      $scope.contactList = res;
+      $scope.contact = '';
     })
+}; //end refresh function
 
+refresh();
 
-}]);
+    //ADD CONTACT
+    //retrieves data from form and sends POST request
+    $scope.addContact = function(){
+      //DATA sent to server
+      console.log($scope.newContact);
+      //POST request to server
+      $http.post('/contactList', $scope.newContact).success(function(res){
+        //DATA received from server (upon success)
+        console.log(res);
+        refresh();
+      });
+    }//end addContact function
+
+    //REMOVE CONTACT
+    $scope.remove = function(id){
+      console.log(id);
+      //DELETE request to server
+      $http.delete('/contactList/' + id).success(function(res){
+        refresh();
+      })
+    }
+
+}]); //end myApp controller
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var app = express();
-var mongoose = require('mongoose');
-// var bodyParser = require('body-parser');
-var mongojs = require('mongojs');
-var db = mongojs('contactList',['contactList'])
+var express    = require('express');
+var mongoose   = require('mongoose');
+var bodyParser = require('body-parser');
+var mongojs    = require('mongojs'); //communicate with mongoDB
+var db         = mongojs('contactList',['contactList'])
+var app        = express();
 
 // //Contact Schema
 // var Schema = mongoose.Schema;
@@ -26,6 +26,8 @@ var db = mongojs('contactList',['contactList'])
 
 //Tells our app where to find our static pages
 app.use(express.static(__dirname + '/public'));
+//Use bodyparser to parse through body data received from form
+app.use(bodyParser.json())
 
 //GET request from $http.get /contactList route
 app.get('/contactList', function(req,res){
@@ -36,7 +38,23 @@ app.get('/contactList', function(req,res){
     console.log(docs);
     res.json(docs)
   })
+})
 
+//POST request from $http.post
+app.post('/contactList', function(req,res){
+  console.log(req.body);
+  //Doing two things: saves form data into db and the res.json sends the data to controller
+  db.contactList.insert(req.body, function(err, doc){
+    res.json(doc)
+  })
+})
+
+app.delete('/contactList/:id', function(req,res){
+  var id = req.params.id;
+  console.log(id);
+  db.contactList.remove({_id: mongojs.ObjectId(id)}, function(err,doc){
+    res.json(doc);
+  })
 })
 
 app.listen('3000');
